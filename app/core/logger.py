613b9476c589f9f9c045d2ff030ea83af7422e99
Nootey <poiskusni@gmail.com>
2025-12-10 09:16:07 +0100

    chore: make logger properly auto rotate name each day

--- app/core/logger.py
+++ app/core/logger.py
@@ -1,9 +1,9 @@
 import logging
 from datetime import datetime
+from logging.handlers import TimedRotatingFileHandler
 from pathlib import Path
 
 from pythonjsonlogger import jsonlogger
-
 
 class AppLogger:
     def __init__(
@@ -16,18 +16,27 @@ class AppLogger:
         # Ensure log directory exists
         Path(log_dir).mkdir(parents=True, exist_ok=True)
 
-        # Generate log filename with today's date
-        date_str = datetime.now().strftime("%Y-%m-%d")
-        log_file = Path(log_dir) / f"app-{date_str}.log"
-
         self.logger = logging.getLogger(name)
         self.logger.setLevel(level)
         self.logger.propagate = False  # avoid duplicate logs
 
         # Prevent duplicate handlers
         if not self.logger.handlers:
+
+            # Base file
+            log_file = Path(log_dir) / "app.log"
+
             # --- File handler (JSON format)
-            file_handler = logging.FileHandler(filename=log_file)
+            file_handler = TimedRotatingFileHandler(
+                filename=log_file,
+                when="midnight",
+                interval=1,
+                # backupCount=30,
+                encoding="utf-8",
+                utc=False,
+            )
+
+            file_handler.suffix = "%Y-%m-%d"
             file_formatter = jsonlogger.JsonFormatter(
                 "%(asctime)s %(levelname)s %(name)s %(message)s"
             )
